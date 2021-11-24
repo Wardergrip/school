f3cde4b8637b4aaa5ef9b73a1a5a7d83430b17b6
Reï Messely <rei.messely@gmail.com>
2021-11-24 09:35:41 +0100

    Finished BasicAlgorithms exercise

--- 1DAE22_08_Messely_Reï/BasicAlgorithms/BasicAlgorithms.cpp
+++ 1DAE22_08_Messely_Reï/BasicAlgorithms/BasicAlgorithms.cpp
@@ -26,6 +26,11 @@ int arr2[arr2Size]{ 40,-8,6,3,-8 };
 int main()
 {
     TestCount();
+    std::cout << std::endl;
+    TestMinMax();
+    TestSwap();
+    TestShuffle();
+    TestBubbleSort();
 }
 
 int Count(int* pArray, int size, int value)
@@ -72,6 +77,18 @@ void Shuffle(int* pArray, int size, int swaps)
         pArray[idx2] = a;
     }
 }
+void BubbleSort(int* pArray, int size)
+{
+    for (int j{ 0 }; j < size; j++)
+    {
+        int n{ size - j };
+        for (int i{ 0 }; i < n; i++)
+        {
+            if (i + 1 == size);
+            else if (pArray[i] > pArray[i + 1]) Swap(pArray, size, i+1, i);
+        }
+    }
+}
 
 void TestCount()
 {
@@ -94,16 +111,84 @@ void TestMinMax()
     {
         std::cout << arr1[i] << " ";
     }
+    std::cout << "\nLowest value in this array is: " << MinElement(arr1, arr1Size) << "\n";
+    std::cout << "Highest value in this array is: " << MaxElement(arr1, arr1Size) << "\n";
+    std::cout << std::endl;
+    for (int i{ 0 }; i < arr2Size; i++)
+    {
+        std::cout << arr2[i] << " ";
+    }
+    std::cout << "\nLowest value in this array is: " << MinElement(arr2, arr2Size) << "\n";
+    std::cout << "Highest value in this array is: " << MaxElement(arr2, arr2Size) << "\n";
 }
 void TestSwap()
 {
-
+    std::cout << "\n== Test of Swap function ==\n";
+    std::cout << "Before swapping:\n";
+    for (int i{ 0 }; i < arr1Size; i++)
+    {
+        std::cout << arr1[i] << " ";
+    }
+    Swap(arr1, arr1Size, 1, 4);
+    std::cout << "\nAfter swapping the second and the fifth element:\n";
+    for (int i{ 0 }; i < arr1Size; i++)
+    {
+        std::cout << arr1[i] << " ";
+    }
+    std::cout << "\nBefore swapping:\n";
+    for (int i{ 0 }; i < arr2Size; i++)
+    {
+        std::cout << arr2[i] << " ";
+    }
+    Swap(arr2, arr2Size, 1, 0);
+    std::cout << "\nAfter swapping the second and the first element:\n";
+    for (int i{ 0 }; i < arr2Size; i++)
+    {
+        std::cout << arr2[i] << " ";
+    }
+    std::cout << std::endl;
 }
 void TestShuffle()
 {
+    std::cout << "\n== Test of Shuffle function ==\n";
+    std::cout << "Before shuffling:\n";
+    for (int i{ 0 }; i < arr1Size; i++)
+    {
+        std::cout << arr1[i] << " ";
+    }
+    Shuffle(arr1, arr1Size, 4);
+    std::cout << "\nAfter shuffling 4 times:\n";
+    for (int i{ 0 }; i < arr1Size; i++)
+    {
+        std::cout << arr1[i] << " ";
+    }
+    std::cout << "\nBefore shuffling:\n";
+    for (int i{ 0 }; i < arr2Size; i++)
+    {
+        std::cout << arr2[i] << " ";
+    }
+    Shuffle(arr2, arr2Size, 40);
+    std::cout << "\nAfter shuffling 40 times:\n";
+    for (int i{ 0 }; i < arr2Size; i++)
+    {
+        std::cout << arr2[i] << " ";
+    }
+    std::cout << std::endl;
 
 }
 void TestBubbleSort()
 {
-
+    std::cout << "\n== Test of BubbleSort function ==\n";
+    std::cout << "Before sorting:\n";
+    for (int i{ 0 }; i < arr1Size; i++)
+    {
+        std::cout << arr1[i] << " ";
+    }
+    BubbleSort(arr1, arr1Size);
+    std::cout << "\nAfter Sorting:\n";
+    for (int i{ 0 }; i < arr1Size; i++)
+    {
+        std::cout << arr1[i] << " ";
+    }
+    std::cout << std::endl;
 }
