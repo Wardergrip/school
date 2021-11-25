db4897873b34d46ea2fe87be623169969b0edbdf
Reï Messely <rei.messely@gmail.com>
2021-11-25 17:15:38 +0100

    Started working on OxoGame exercise

---
+++ 1DAE22_08_Messely_Reï/OxoGame/Game.h
@@ -0,0 +1,41 @@
+#pragma once
+using namespace utils;
+#pragma region gameInformation
+// Set your name and group in the title here
+std::string g_WindowTitle{ "Project name - Name, firstname - 1DAExx" };
+
+// Change the window dimensions here
+float g_WindowWidth{ 500 };
+float g_WindowHeight{ 300 };
+#pragma endregion gameInformation
+
+
+
+#pragma region ownDeclarations
+// Declare your own global variables here
+Texture g_FreeCellTxt;
+Texture g_GreenOTxt;
+Texture g_GreenXTxt;
+Texture g_OXOTxt;
+Texture g_WhiteOTxt;
+Texture g_WhiteXTxt;
+Texture g_WinTxt;
+// Declare your own functions here
+void InitFiles();
+void DeleteTextures();
+#pragma endregion ownDeclarations
+
+#pragma region gameFunctions											
+void Start();
+void Draw();
+void Update(float elapsedSec);
+void End();
+#pragma endregion gameFunctions
+
+#pragma region inputHandling											
+void OnKeyDownEvent(SDL_Keycode key);
+void OnKeyUpEvent(SDL_Keycode key);
+void OnMouseMotionEvent(const SDL_MouseMotionEvent& e);
+void OnMouseDownEvent(const SDL_MouseButtonEvent& e);
+void OnMouseUpEvent(const SDL_MouseButtonEvent& e);
+#pragma endregion inputHandling
