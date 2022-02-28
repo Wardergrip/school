0cebfedac3994ef1472cbf851e62767782ae798f
Wardergrip <rei.messely@gmail.com>
2022-03-01 00:13:20 +0100

    Minor fixes

--- Semester 2/Misc/GameObject.h
+++ Semester 2/Misc/GameObject.h
@@ -1,19 +1,4 @@
 #pragma once
-#pragma region DeleteThis
-struct Point2f
-{
-    Point2f(float X,float Y){x=X;y=Y;};
-    Point2f(){x=0;y=0;};
-    float x;
-    float y;
-};
-struct Rectf
-{
-    Rectf(float LEFT,float BOTTOM,float WIDTH,float HEIGHT){left=LEFT;bottom=BOTTOM;width=WIDTH;height=HEIGHT;};
-    Rectf(){left=0;bottom=0;width=0;height=0;};
-    float left,bottom,width,height;
-};
-#pragma endregion DeleteThis
 class GameObject
 {
 public:
